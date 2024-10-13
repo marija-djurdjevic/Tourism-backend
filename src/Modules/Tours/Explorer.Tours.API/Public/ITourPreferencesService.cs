be1a5dd4e045ce47e81b57a0d22ef3dfb17ee7d3
nemanajZ02 <nemanja.zekanovic002@gmail.com>
2024-10-13 14:21:50 +0200

    feat: added TourPreferences model, Dto and service

---
+++ src/Modules/Tours/Explorer.Tours.API/Public/ITourPreferencesService.cs
@@ -0,0 +1,19 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Public
+{
+    public interface ITourPreferencesService
+    {
+        Result<PagedResult<TourPreferencesDto>> GetPaged(int page, int pageSize);
+        Result<TourPreferencesDto> Create(TourPreferencesDto equipment);
+        Result<TourPreferencesDto> Update(TourPreferencesDto equipment);
+        Result Delete(int id);
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/TourPreferencesService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Core.Domain;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.UseCases
+{
+    public class TourPreferencesService : CrudService<TourPreferencesDto, TourPreferences>, ITourPreferencesService
+    {
+        public TourPreferencesService(ICrudRepository<TourPreferences> repository, IMapper mapper) : base(repository, mapper) { }
+    }
+}
