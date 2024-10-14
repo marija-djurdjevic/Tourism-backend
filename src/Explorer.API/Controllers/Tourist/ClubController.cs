ad1cb5f01379f75cfc6a8d05c230cedba57bfd61
Đorđe Ristić <djordjer88@gmail.com>
2024-10-14 19:31:11 +0200

    feat: infrastructure, controller

---
+++ src/Explorer.API/Controllers/Tourist/ClubController.cs
@@ -0,0 +1,34 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Route("api/administration/club")]
+    public class ClubController : BaseApiController
+    {
+        private readonly IClubService _clubService;
+
+        public ClubController(IClubService clubService)
+        {
+            _clubService = clubService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<ClubDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _clubService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpPost]
+        public ActionResult<ClubDto> Create([FromBody] ClubDto clubDto)
+        {
+            var result = _clubService.Create(clubDto);
+            return CreateResponse(result);
+        }
+    }
+}
