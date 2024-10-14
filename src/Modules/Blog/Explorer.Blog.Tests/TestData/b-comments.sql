8cf62530aa613fed1adbfd688ffbdc2ad05f3587
Zeka5 <zekovicognjen753@gmail.com>
2024-10-14 18:12:51 +0200

    feat: created test for comments crud

---
+++ src/Modules/Blog/Explorer.Blog.Tests/TestData/b-comments.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO blog."Comment"(
+    "Id", "AuthorId", "Text", "CreationDate", "EditDate")
+VALUES (-1, 0, 'Komentarcina', '2024-10-14 15:23:44.223+02', '2024-10-14 15:23:44.223+02');
+INSERT INTO blog."Comment"(
+    "Id", "AuthorId", "Text", "CreationDate", "EditDate")
+VALUES (-2, 1, 'No koment', '2024-10-14 15:23:44.223+02', '2024-10-14 15:23:44.223+02');
+INSERT INTO blog."Comment"(
+    "Id", "AuthorId", "Text", "CreationDate", "EditDate")
+VALUES (-3, 2, 'Koment', '2024-10-14 15:23:44.223+02', '2024-10-14 15:23:44.223+02');
