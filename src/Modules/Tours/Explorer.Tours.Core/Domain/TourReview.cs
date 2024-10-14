20cb444a63b855fbeec5ec23505e7bdff73b5df5
mimasekaric <mimasekaric@gmail.com>
2024-10-14 17:50:46 +0200

    feat: Added TourReview model, Dto and service

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourReview.cs
@@ -0,0 +1,45 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class TourReview : Entity
+    {
+        public int Id { get; private set; }
+        public int Grade { get; private set; }
+        public string Comment { get; private set; }
+
+        public int TourId { get; private set; }
+
+        public int UserId { get; private set; }
+
+        public List<string> Images { get; private set; }
+
+        public DateOnly TourVisitDate { get; private set; }
+
+        public DateOnly TourReviewDate { get; private set; }
+
+        public TourReview(int id, int grade, string comment, int tourId, int userId, List<string>images, DateOnly tourVisitDate, DateOnly tourReviewDate)
+        {
+            if (string.IsNullOrWhiteSpace(comment)) throw new ArgumentException("Invalid Comment.");
+            if (images == null || images.Count == 0) throw new ArgumentException("Tour must have at least one image.");
+            Id = id;
+            Grade = grade;
+            Comment = comment;
+            TourId = tourId;
+            UserId = userId;
+            Images = images;
+            TourVisitDate = tourVisitDate;
+            TourReviewDate = tourReviewDate;
+
+        }
+    }
+
+}
