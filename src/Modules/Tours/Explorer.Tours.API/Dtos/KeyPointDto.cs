67ce2e902d4bf43c018e015b8b0b073ec8a2a029
Marija Djurdjevic <masadjurdjevic02@gmail.com>
2024-10-14 15:06:25 +0200

    feat: add key point model, dto and service

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/KeyPointDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class KeyPointDto
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String ImagePath { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/KeyPoint.cs
@@ -0,0 +1,37 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class KeyPoint : Entity
+    {
+        public int Id { get; private set; }
+        public String Name { get; private set; }
+        public String Description { get; private set; }
+        public String ImagePath { get; private set; }
+        public double Longitude { get; private set; }
+        public double Latitude { get; private set;}
+        public int AuthorId { get; private set; }
+
+        public KeyPoint(int id, string name, string description, string imagePath, double longitude, double latitude, int authorId)
+        {
+            if (id <= 0) throw new ArgumentException("Invalid Id.");
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Invalid Name.");
+            if (string.IsNullOrWhiteSpace(description)) throw new ArgumentException("Invalid Description.");
+            if (string.IsNullOrWhiteSpace(imagePath)) throw new ArgumentException("Invalid Image Path.");
+            if (longitude < -180 || longitude > 180) throw new ArgumentException("Longitude must be between -180 and 180.");
+            if (latitude < -90 || latitude > 90) throw new ArgumentException("Latitude must be between -90 and 90.");
+            if (authorId <= 0) throw new ArgumentException("Invalid Author Id.");
+
+            Id = id;
+            Name = name;
+            Description = description;
+            ImagePath = imagePath;
+            Longitude = longitude;
+            Latitude = latitude;
+            AuthorId = authorId;
+        }
+
+
+
+    }
+}
