e67f46ab374e0074a818678a7c0f0d28c29d00a3
Ivan Radmilovic <radmilovicivan@gmail.com>
2024-10-14 15:27:15 +0200

    feat: infrastracture and database

--- src/Modules/Blog/Explorer.Blog.Core/UseCases/BlogService.cs
+++ src/Modules/Blog/Explorer.Blog.Core/UseCases/BlogService.cs
@@ -14,32 +14,6 @@ namespace Explorer.Blog.Core.UseCases
 {
     public class BlogService : CrudService<BlogDto, Blogs>, IBlogService
     {
-
         public BlogService(ICrudRepository<Blogs> repository, IMapper mapper) : base(repository, mapper){}
-
-        public  Result<BlogDto> Get(int id)
-        {
-            return base.Get(id);
-        }
-
-        public override Result<BlogDto> Create(BlogDto blog)
-        {
-            return base.Create(blog);
-        }
-
-        public override Result<BlogDto> Update(BlogDto blog)
-        {
-            return base.Update(blog);
-        }
-
-        public override Result Delete(int id)
-        {
-            return base.Delete(id);
-        }
-
-        public  Result<PagedResult<BlogDto>> GetPaged(int page, int pageSize)
-        {
-            return base.GetPaged(page, pageSize);
-        }
     }
 }
