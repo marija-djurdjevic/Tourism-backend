3436e9dea69154c3ba342f6b60958a264dbfe48d
Paola Mitrovic <paolamitrovic02@gmail.com>
2024-10-16 04:56:40 +0200

    feat: Added dto, interface and service

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Public/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.API.Dtos;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.API.Public
+{
+    public interface IAccountService
+    {
+        Result<PagedResult<AccountReviewDto>> GetPaged(int page, int pageSize);
+        Result<AccountReviewDto> Update(AccountReviewDto accountReview);
+    }
+}
