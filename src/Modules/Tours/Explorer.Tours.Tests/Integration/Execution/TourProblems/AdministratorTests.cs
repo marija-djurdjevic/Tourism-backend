4dc7809a1aff25e611ace8701faf74f3516ba848
nemanajZ02 <nemanja.zekanovic002@gmail.com>
2024-10-30 00:33:35 +0100

    tests partially fixed

--- src/Modules/Tours/Explorer.Tours.Tests/Integration/Execution/TourProblems/AdministratorTests.cs
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/Execution/TourProblems/AdministratorTests.cs
@@ -2,15 +2,19 @@
 using Explorer.Tours.API.Dtos.TourProblemDtos;
 using Explorer.Tours.API.Public.Administration;
 using Explorer.Tours.API.Public.Execution;
+using Explorer.Tours.Core.Domain.TourProblems;
 using Explorer.Tours.Infrastructure.Database;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
+using ProblemDetails = Explorer.Tours.Core.Domain.TourProblems.ProblemDetails;
+using ProblemStatus = Explorer.Tours.API.Dtos.TourProblemDtos.ProblemStatus;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProblemCategory = Explorer.Tours.Core.Domain.TourProblems.ProblemCategory;
 
 namespace Explorer.Tours.Tests.Integration.Execution.TourProblems
 {
@@ -59,12 +63,86 @@ namespace Explorer.Tours.Tests.Integration.Execution.TourProblems
                 var updatedEntity = dbContext.TourProblems.FirstOrDefault(tp => tp.Id == initialDto.Id);
                 updatedEntity.ShouldNotBeNull();
         }
-            private static TourProblemController CreateController(IServiceScope scope)
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        public void GetAll_ReturnsPagedResultOfTourProblemDtos(int tourId, int touristId)
+        {
+            // Arrange
+            var details = new ProblemDetails(Core.Domain.TourProblems.ProblemCategory.RoadObstacles, 1, "drvo na putu", new DateTime(2024, 10, 29, 10, 53, 25));
+            var detailsDto = new ProblemDetailsDto
             {
-                return new TourProblemController(scope.ServiceProvider.GetRequiredService<ITourProblemService>(), scope.ServiceProvider.GetRequiredService<ITourService>())
-                {
+                Category = (API.Dtos.TourProblemDtos.ProblemCategory)API.Dtos.TourProblemDtos.ProblemCategory.RoadObstacles,
+                Time = new DateTime(2024, 10, 29, 10, 53, 25),
+                ProblemPriority = 1,
+                Explanation = "drvo na putu"
+            };
+
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dbContext = scope.ServiceProvider.GetRequiredService<ToursContext>();
+
+
+            var tourProblemDto = new TourProblemDto
+            {
+                TourId = tourId,
+                //TouristId = 1,
+                Details = detailsDto,
+                Status = ProblemStatus.Pending
+            };
+
+
+            // Act
+            var result = controller.GetAll().Result as ObjectResult;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(200);
+
+        }
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        public void SetDeadline_SetsDeadlineAndReturnsUpdatedTourProblem(int tourId, int touristId)
+        {
+            // Arrange
+            var details = new ProblemDetails(Core.Domain.TourProblems.ProblemCategory.RoadObstacles, 1, "drvo na putu", new DateTime(2024, 10, 29, 10, 53, 25));
+            var detailsDto = new ProblemDetailsDto
+            {
+                Category = (API.Dtos.TourProblemDtos.ProblemCategory)API.Dtos.TourProblemDtos.ProblemCategory.RoadObstacles,
+                Time = new DateTime(2024, 10, 29, 10, 53, 25),
+                ProblemPriority = 1,
+                Explanation = "drvo na putu"
+            };
+
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dbContext = scope.ServiceProvider.GetRequiredService<ToursContext>();
+
+            var tourProblemDto = new TourProblemDto
+            {
+                TourId = tourId,
+                Details = detailsDto,
+                Status = ProblemStatus.Pending
+            };
+
+            var deadline = DateTime.Now.AddDays(1);
+            var result = (ObjectResult)controller.SetDeadline((int)tourProblemDto.Id, deadline).Result;
+
+
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(200);
+            var storedEntity = dbContext.TourProblems.FirstOrDefault(t => t.Id == tourId);
+            storedEntity.Deadline.ShouldBe(deadline);
+
+        }
+        private static TourProblemController CreateController(IServiceScope scope)
+        {
+            return new TourProblemController(scope.ServiceProvider.GetRequiredService<ITourProblemService>(), scope.ServiceProvider.GetRequiredService<ITourService>())
+            {
                     ControllerContext = BuildContext("-1")
-                };
-            }
+            };
+        }
     }
 }
