f00673a4e5e61c64948ce68a4a6740b6757d38c3
mimasekaric <mimasekaric@gmail.com>
2024-10-30 00:00:49 +0100

    implemented backend and tests

---
+++ src/Explorer.API/Controllers/Administrator/TourProblemController.cs
@@ -0,0 +1,39 @@
+﻿using Explorer.Stakeholders.API.Dtos;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Dtos.TourProblemDtos;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.API.Public.Execution;
+using FluentResults;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Administrator
+{
+    
+    [Authorize(Policy = "administratorPolicy")]
+    [Route("api/administrator/problem")]
+    public class TourProblemController : BaseApiController
+    {
+
+        private readonly ITourProblemService _tourProblemService;
+        private readonly ITourService _tourService;
+
+        public TourProblemController(ITourProblemService tourProblemService, ITourService tourService)
+        {
+            _tourProblemService = tourProblemService;
+            _tourService = tourService;
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<TourPreferencesDto> Update([FromBody] TourProblemDto tourProblem)
+        {
+            if(tourProblem.Deadline < DateTime.UtcNow)
+            {
+                var result = _tourProblemService.CloseProblem(tourProblem);
+                return CreateResponse(result);
+            }
+            return CreateResponse(Result.Fail("deadline hasn't passed."));  
+        }
+
+    }
+}
