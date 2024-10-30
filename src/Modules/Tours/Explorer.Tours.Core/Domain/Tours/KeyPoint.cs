a94dd560a7d7e059c4f0a08819455950fc4d30f8
Paola Mitrovic <paolamitrovic02@gmail.com>
2024-10-30 17:45:00 +0100

    feat: Added logic for desired distance

--- src/Modules/Tours/Explorer.Tours.Core/Domain/Tours/KeyPoint.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Tours/KeyPoint.cs
@@ -4,27 +4,34 @@ namespace Explorer.Tours.Core.Domain.Tours
 {
     public class KeyPoint : Entity
     {
-        public int Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
         public string ImagePath { get; private set; }
         public int TourId { get; private set; }
         public Coordinates Coordinates { get; private set; }
-        public KeyPoint(int id, string name, string description, string imagePath, int tourId)
+        public KeyPoint(string name, string description, string imagePath, int tourId)
         {
-            if (id < 0) throw new ArgumentException("Invalid Id.");
             if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Invalid Name.");
             if (string.IsNullOrWhiteSpace(description)) throw new ArgumentException("Invalid Description.");
             if (string.IsNullOrWhiteSpace(imagePath)) throw new ArgumentException("Invalid Image Path.");
             if (tourId < 0) throw new ArgumentException("Invalid Author Id.");
 
-            Id = id;
             Name = name;
             Description = description;
             ImagePath = imagePath;
             TourId = tourId;
         }
 
+        public double GetDistance(Coordinates desiredCoordinates)
+        {
+            return Math.Sqrt(Math.Pow(desiredCoordinates.Latitude - Coordinates.Latitude, 2) + Math.Pow(desiredCoordinates.Longitude - Coordinates.Longitude, 2));
+        }
+
+        public bool IsInDesiredDistance(Coordinates desiredCoordinates, double distance)
+        {
+            var actualDistance = GetDistance(desiredCoordinates);
+            return actualDistance < distance;
+        }
 
 
     }
--- src/Modules/Tours/Explorer.Tours.Core/UseCases/Authoring/TourService.cs
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Authoring/TourService.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.Core.Domain;
+using Explorer.Stakeholders.Core.Domain.RepositoryInterfaces;
 using Explorer.Tours.API.Dtos.TourLifeCycleDtos;
 using Explorer.Tours.API.Public.Authoring;
+using Explorer.Tours.Core.Domain.RepositoryInterfaces;
 using Explorer.Tours.Core.Domain.Tours;
 using FluentResults;
 using System;
@@ -14,8 +17,29 @@ namespace Explorer.Tours.Core.UseCases.Authoring
 {
     public class TourService : CrudService<TourDto, Tour>, ITourService
     {
+
+        //private readonly IKeyPointService _keyPointService;
+        //private ICrudRepository<Tour> _tourRepository;
+        //private readonly ITourRepository _tourRepository;
         public TourService(ICrudRepository<Tour> repository, IMapper mapper) : base(repository, mapper) { }
 
+        //public Result<TourDto> SearchTours(Coordinates coordinates, double distance)
+        //{
+        //    HashSet<int> tourIds = new HashSet<int>();
+        //    var keyPoints = _keyPointService.GetAll();
+        //    foreach (var kp in keyPoints)
+        //    {
+        //        if (true) //kp.IsInDesiredDistance(coordinates,distance)
+        //            tourIds.Add(kp.TourId);
+        //    }
+        //    Result<TourDto> result = new Result<TourDto>();
+        //    foreach (var tourId in tourIds)
+        //    {
+        //        var tour = _tourRepository.Get(tourId);
+        //        result.WithValue(tour);
+        //    }
+        //    return result;
+        //}
         public Result<List<TourDto>> GetByAuthorId(int id)
         {
             throw new NotImplementedException();
