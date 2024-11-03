b2c5326251af4667a70dc8fb1ad4be4a67e0d196
Relja Vranjes <reljavranjes02@gmail.com>
2024-11-03 14:49:03 +0100

    added keyPoints get method

--- src/Explorer.API/Controllers/Tourist/TourController.cs
+++ src/Explorer.API/Controllers/Tourist/TourController.cs
@@ -15,16 +15,25 @@ namespace Explorer.API.Controllers.Tourist
     public class TourController : BaseApiController
     {
         private readonly ITourService _tourService;
+        private readonly IKeyPointService _keyPointService;
 
-        public TourController(ITourService tourService)
+        public TourController(ITourService tourService, IKeyPointService keyPointService)
         {
             _tourService = tourService;
+            _keyPointService = keyPointService;
         }
 
         [HttpGet]
         public ActionResult<PagedResult<TourDto>> GetAllPublished([FromQuery] int page, [FromQuery] int pageSize)
         {
             var result = _tourService.GetAllPublished(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpGet("keyPoints")]
+        public ActionResult<PagedResult<KeyPointDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _keyPointService.GetPaged(page, pageSize);
             return CreateResponse(result);
         }
 
