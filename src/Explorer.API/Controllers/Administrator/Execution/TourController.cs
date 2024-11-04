9d38ef9bd357e6c14a02bd00d1e666c8a30fe9b2
nemanajZ02 <nemanja.zekanovic002@gmail.com>
2024-11-04 18:43:04 +0100

    fixed Administrator's TourController

--- src/Explorer.API/Controllers/Administrator/Execution/TourController.cs
+++ src/Explorer.API/Controllers/Administrator/Execution/TourController.cs
@@ -1,4 +1,5 @@
-﻿using Explorer.Tours.API.Dtos.TourLifecycleDtos;
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos.TourLifecycleDtos;
 using Explorer.Tours.API.Public.Authoring;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -17,9 +18,16 @@ namespace Explorer.API.Controllers.Administrator.Execution
         }
 
         [HttpGet("all")]
-        public ActionResult<List<TourDto>> GetAllTours()
+        public ActionResult<PagedResult<TourDto>> GetAllTours()
         {
             var result = _tourService.GetAllToursWithKeyPoints();
+            return CreateResponse(result);
+        }
+
+        [HttpGet("allTours")]
+        public ActionResult<PagedResult<TourDto>> GetAllAdmin()
+        {
+            var result = _tourService.GetPaged(0, 0);
             return CreateResponse(result);
         }
     }
