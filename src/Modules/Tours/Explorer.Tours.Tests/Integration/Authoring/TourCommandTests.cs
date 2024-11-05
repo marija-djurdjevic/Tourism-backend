e2a64a50d920691be958df4043100036f3d755dd
janaajankovic <janajankoviic01@gmail.com>
2024-11-06 00:00:07 +0100

    final tests changes

--- src/Modules/Tours/Explorer.Tours.Tests/Integration/Authoring/TourCommandTests.cs
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/Authoring/TourCommandTests.cs
@@ -67,8 +67,11 @@ namespace Explorer.Tours.Tests.Integration.Authoring
             using var scope = Factory.Services.CreateScope();
             var controller = CreateController(scope);
             var dbContext = scope.ServiceProvider.GetRequiredService<ToursContext>();
-            var updatedTour = new TourDto { Id = -1, AuthorId = 1, Description = "sdadsa", AverageScore = 0, Price = 0, Tags = "sss", Name = "Publish Test Tour", Status = TourStatus.Archived };
-
+            var updatedTour = new TourDto { Id = -1, AuthorId = 1, Description = "sdadsa", AverageScore = 0, Price = 100, Tags = "sss", Name = "Publish Test Tour", Status = TourStatus.Archived };
+            updatedTour.KeyPoints = new List<KeyPointDto>();
+            updatedTour.KeyPoints.Add(new KeyPointDto { Id = 100, Description = "kp1", ImagePath = "...", Latitude = 2.2, Longitude = 3.3, Name = "KP1", TourId = -1 });
+            updatedTour.KeyPoints.Add(new KeyPointDto { Id = 200, Description = "kp2", ImagePath = "...", Latitude = 2.2, Longitude = 3.3, Name = "KP2", TourId = -1 });
+            updatedTour.TransportInfo = new TransportInfoDto() { Distance = 2.0, Time = 100, Transport = TransportInfoDto.TransportType.Car }; 
             // Act
             var result = ((ObjectResult)controller.Publish(updatedTour).Result)?.Value as TourDto;
 
