ca90d42553e944945c72f97c8b52b9bfad8447be
Ivan Radmilovic <radmilovicivan@gmail.com>
2024-11-09 12:02:35 +0100

    all tests for blog passing

--- src/Modules/Blog/Explorer.Blog.Tests/Integration/BlogCommandTests.cs
+++ src/Modules/Blog/Explorer.Blog.Tests/Integration/BlogCommandTests.cs
@@ -22,8 +22,41 @@ namespace Explorer.Blog.Tests.Integration
     {
         public BlogCommandTests(BlogTestFactory factory) : base(factory){}
 
-        [Fact]
-        public void Creates()
+        public static IEnumerable<object[]> GetTestData()
+        {
+            yield return new object[]
+            {
+                "NASLOVCINA", 
+                new List<VoteDto>
+                {
+                    new VoteDto { AuthorId = 101, CreationDate = DateTime.UtcNow, Value = true },
+                    new VoteDto { AuthorId = 102, CreationDate = DateTime.UtcNow, Value = true }
+                }, 
+                456, 
+                "dadadsdadsdasdasdas", 
+                DateTime.UtcNow, 
+                "slika.png", 
+                1 
+            };
+
+            yield return new object[]
+            {
+                "AnotherTitle", 
+                new List<VoteDto>
+                {
+                    new VoteDto { AuthorId = 201, CreationDate = DateTime.UtcNow, Value = true }
+                }, 
+                789, 
+                "AnotherDescription", 
+                DateTime.UtcNow, 
+                "anotherImage.png", 
+                2 
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void Create(string blogTitle, List<VoteDto> votes, int voteAuthorId,string description, DateTime voteCreationDate, string img, int status)
         {
             // Arrange
             using var scope = Factory.Services.CreateScope();
@@ -31,19 +64,12 @@ namespace Explorer.Blog.Tests.Integration
             var dbContext = scope.ServiceProvider.GetRequiredService<BlogContext>();
             var newEntity = new BlogDto
             {
-                Title = "NASLOVCINA",
-                AuthorId = 1 ,
-                Votes = new List<VoteDto> 
-                {
-                    new() {
-                        AuthorId = 123,
-                        CreationDate = DateTime.Parse("2024-11-01T00:00:00"),
-                        Value = true
-                    }
-                },
-                Description = "Description",
-                CreationDate = DateTime.UtcNow,
-                Image = "image.png",
+                Title = blogTitle,
+                AuthorId = voteAuthorId,
+                Votes = votes,
+                Description = description,
+                CreationDate = voteCreationDate,
+                Image = img,
                 Status = BlogDto.BlogStatus.Published,
             };
 
@@ -51,7 +77,7 @@ namespace Explorer.Blog.Tests.Integration
             var result = ((ObjectResult)controller.Create(newEntity).Result)?.Value as BlogDto;
 
             // Assert - Response
-            
+
             result.ShouldNotBeNull();
             result.Id.ShouldNotBe(0);
             result.Title.ShouldBe(newEntity.Title);
@@ -105,6 +131,68 @@ namespace Explorer.Blog.Tests.Integration
                 v.AuthorId == newVote.AuthorId &&
                 v.CreationDate == newVote.CreationDate &&
                 v.Value == newVote.Value);
+        }
+
+        [Theory]
+        [InlineData("Title1", 123, "2024-11-06T00:00:00", false)]
+        public void UpdateSingleVoteToExistingBlog_ThroughController(string blogTitle, int voteAuthorId, string voteCreationDate, bool voteValue)
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dbContext = scope.ServiceProvider.GetRequiredService<BlogContext>();
+            var existingBlog = dbContext.Blogs.FirstOrDefault(b => b.Title == blogTitle);
+            existingBlog.ShouldNotBeNull();
+
+            var newVote = new VoteDto
+            {
+                AuthorId = voteAuthorId,
+                CreationDate = DateTime.Parse(voteCreationDate),
+                Value = voteValue
+            };
+
+            // Act
+            var updateVoteResult = controller.AddVote(Convert.ToInt32(existingBlog.Id), newVote).Result as ObjectResult;
+            var updatedBlog = dbContext.Blogs.FirstOrDefault(b => b.Title == blogTitle);
+
+            // Assert - Response and Database
+            updateVoteResult.ShouldNotBeNull();
+            updateVoteResult.StatusCode.ShouldBe(200);
+
+            updatedBlog.ShouldNotBeNull();
+            updatedBlog.Votes.ShouldContain(v =>
+                v.AuthorId == newVote.AuthorId &&
+                v.CreationDate == newVote.CreationDate &&
+                v.Value == newVote.Value);
+        }
+
+        [Theory]
+        [InlineData("Title1", 123, "2024-11-06T00:00:00", false)]
+        public void DeleteSingleVoteToExistingBlog_ThroughController(string blogTitle, int voteAuthorId, string voteCreationDate, bool voteValue)
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dbContext = scope.ServiceProvider.GetRequiredService<BlogContext>();
+            var existingBlog = dbContext.Blogs.FirstOrDefault(b => b.Title == blogTitle);
+            existingBlog.ShouldNotBeNull();
+
+            var newVote = new VoteDto
+            {
+                AuthorId = voteAuthorId,
+                CreationDate = DateTime.Parse(voteCreationDate),
+                Value = voteValue
+            };
+
+            // Act
+            var removeVoteResult = controller.RemoveVote(Convert.ToInt32(existingBlog.Id), voteAuthorId).Result as ObjectResult;
+            var updatedBlog = dbContext.Blogs.FirstOrDefault(b => b.Title == blogTitle);
+
+            // Assert - Response and Database
+            removeVoteResult.ShouldNotBeNull();
+            removeVoteResult.StatusCode.ShouldBe(200);
+
+            Assert.Empty(updatedBlog.Votes);
         }
 
         [Fact]
--- src/Modules/Blog/Explorer.Blog.Tests/TestData/b-blogs.sql
+++ src/Modules/Blog/Explorer.Blog.Tests/TestData/b-blogs.sql
@@ -1,15 +1,15 @@
 ﻿INSERT INTO blog."Blogs"(
     "Id", "Votes", "Title", "AuthorId", "Description", "CreationDate", "Image", "Status"
 ) 
-VALUES (-1, '{"AuthorId": 123, "CreationDate": "2024-11-01T00:00:00", "Value": true}', 'Title1', 1, 'blogara1', '2024-10-14 15:23:44.223+02', 'slika1', 1);
+VALUES (-1, '[{{"AuthorId": 123, "CreationDate": "2024-11-01T00:00:00", "Value": true}}]', 'Title1', 1, 'blogara1', '2024-10-14 15:23:44.223+02', 'slika1', 1);
 
 INSERT INTO blog."Blogs"(
     "Id", "Votes", "Title", "AuthorId", "Description", "CreationDate", "Image", "Status"
 ) 
-VALUES (-2, '{"AuthorId": 123, "CreationDate": "2024-11-01T00:00:00", "Value": true}', 'Title2', 1, 'blogara2', '2024-10-14 15:23:44.223+02', 'slika1', 1);
+VALUES (-2, '[{{"AuthorId": 123, "CreationDate": "2024-11-01T00:00:00", "Value": true}}]', 'Title2', 1, 'blogara2', '2024-10-14 15:23:44.223+02', 'slika1', 1);
 
 INSERT INTO blog."Blogs"(
     "Id", "Votes", "Title", "AuthorId", "Description", "CreationDate", "Image", "Status"
 ) 
-VALUES (-3, '{"AuthorId": 123, "CreationDate": "2024-11-01T00:00:00", "Value": true}', 'Title3', 1, 'blogara3', '2024-10-14 15:23:44.223+02', 'slika1', 1);
+VALUES (-3, '[{{"AuthorId": 123, "CreationDate": "2024-11-01T00:00:00", "Value": true}}]', 'Title3', 1, 'blogara3', '2024-10-14 15:23:44.223+02', 'slika1', 1);
 
