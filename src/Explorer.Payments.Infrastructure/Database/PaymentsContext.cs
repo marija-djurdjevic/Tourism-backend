10dcc481d3585e9893ecdf0127f7ee7e92c60528
Relja Vranjes <reljavranjes02@gmail.com>
2024-11-17 18:07:42 +0100

    added payments module

---
+++ src/Explorer.Payments.Infrastructure/Database/PaymentsContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Payments.Infrastructure.Database
+{
+    public class PaymentsContext : DbContext
+    {
+        //DbSet svih entiteta
+
+        public PaymentsContext(DbContextOptions<PaymentsContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("payments");
+        }
+    }
+}
---
+++ src/Explorer.Payments.Infrastructure/PaymentsStartup.cs
@@ -0,0 +1,33 @@
+﻿using Explorer.Payments.Core.Mappers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Payments.Infrastructure
+{
+    public static class PaymentsStartup
+    {
+        public static IServiceCollection ConfigurePaymentsModule(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(PaymentsProfile).Assembly); // Preduslov da imamo ovu liniju koda je da smo definisali već Profile klasu u Core/Mappers
+            SetupCore(services);
+            SetupInfrastructure(services);
+            return services;
+        }
+
+        // Dependencies for services
+        private static void SetupCore(IServiceCollection services)
+        {
+            
+        }
+
+        // Dependencies for repositories
+        private static void SetupInfrastructure(IServiceCollection services)
+        {
+            
+        }
+    }
+}
