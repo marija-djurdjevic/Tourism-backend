375c99dc6e6f724f4ddc8a130d2c867823371c71
Adrijan Radjevic <adrijan.radjevic@gmail.com>
2024-11-23 12:45:07 +0100

    implemented wallet and made tests work

---
+++ src/Explorer.Payments.API/Dtos/WalletDtos/WalletDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Payments.API.Dtos.WalletDtos
+{
+    public class WalletDto
+    {
+        public int TouristId { get; set; }
+        public double Balance { get; set; }
+        
+        public WalletDto(int touristId) { 
+            TouristId = touristId;
+            Balance = 0;
+        }
+    
+    }
+}
