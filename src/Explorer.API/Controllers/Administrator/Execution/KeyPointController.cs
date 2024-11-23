3a6ba6fb06bf7e84ef77eff9e76a16e527b1f617
jelena-ra <jelena.rdsc02@gmail.com>
2024-11-23 23:15:54 +0100

    Rejecting public keypoint requests functionallity added

--- src/Explorer.API/Controllers/Administrator/Execution/KeyPointController.cs
+++ src/Explorer.API/Controllers/Administrator/Execution/KeyPointController.cs
@@ -1,8 +1,11 @@
 ﻿using Explorer.BuildingBlocks.Core.UseCases;
 using Explorer.Tours.API.Dtos;
 using Explorer.Tours.API.Dtos.PublishRequestDtos;
+using Explorer.Tours.API.Dtos.TourProblemDtos;
 using Explorer.Tours.API.Public.Authoring;
+using Explorer.Tours.API.Public.Execution;
 using Explorer.Tours.Core.UseCases.Authoring;
+using FluentResults;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,11 +19,13 @@ namespace Explorer.API.Controllers.Administrator.Execution
 
         private readonly IKeyPointService _keyPointService;
         private readonly IPublishRequestService _publishRequestService;
+        private readonly INotificationService _notificationService;
 
-        public KeyPointController(IKeyPointService keyPointService, IPublishRequestService publishRequestService)
+        public KeyPointController(IKeyPointService keyPointService, IPublishRequestService publishRequestService, INotificationService notificationService)
         {
             _keyPointService = keyPointService;
             _publishRequestService = publishRequestService;
+            _notificationService = notificationService;
         }
 
         [HttpGet]
@@ -39,11 +44,26 @@ namespace Explorer.API.Controllers.Administrator.Execution
             if (publishRequest.Status == PublishRequestDto.RegistrationRequestStatus.Accepted)
             {
                 _keyPointService.PublishKeyPoint(publishRequest.EntityId);
+                
             }
             //ovdje dodaj za decline
+            if (publishRequest.Status == PublishRequestDto.RegistrationRequestStatus.Rejected)
+            {
+                _keyPointService.PublishKeyPoint(publishRequest.EntityId);
+                notifyAccepted(result.Value);
+            }
 
-           
             return CreateResponse(result);
         }
+
+
+    
+
+    private void notifyAccepted(PublishRequestDto req)
+    {
+        int tourAuthorId = req.AuthorId;
+        string content = $"Your public keypoint request has been rejected";
+        _notificationService.Create(new NotificationDto(content, NotificationType.PublicRequest, req.Id, tourAuthorId, false));
     }
+}
 }
