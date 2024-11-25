5d400a86f60cd470dc13e2d09d00f137625b6b5c
Zeka5 <zekovicognjen753@gmail.com>
2024-11-25 22:26:15 +0100

    feat: added bundles CRUD

---
+++ src/Explorer.API/Controllers/Author/BundleController.cs
@@ -0,0 +1,53 @@
+﻿using Explorer.Blog.API.Dtos;
+using Explorer.Blog.API.Public;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Payments.API.Public.Shopping;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Explorer.Payments.API.Dtos.ShoppingDtos;
+
+namespace Explorer.API.Controllers.Author
+{
+    [Authorize(Policy = "authorPolicy")]
+    [Route("api/author/bundle")]
+    public class BundleController : BaseApiController
+    {
+        private readonly IBundleService bundleService;
+
+        public BundleController(IBundleService bundleService)
+        {
+            this.bundleService = bundleService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<BundleDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = bundleService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpGet("{authorId}")]
+        public ActionResult<BundleDto> GetByAuthorId(int authorId)
+        {
+            var result = bundleService.GetByAuthorId(authorId);
+            return CreateResponse(result);
+        }
+
+        [HttpPost]
+        public ActionResult<BundleDto> Create([FromBody] BundleDto bundle)
+        {
+            var result = bundleService.Create(bundle);
+            return CreateResponse(result);
+        }
+
+        [HttpPut]
+        public ActionResult<BundleDto> Update([FromBody] BundleDto bundle)
+        {
+            var result = bundleService.Update(bundle);
+            return CreateResponse(result);
+        }
+
+    }
+}
---
+++ src/Explorer.Payments.Core/UseCases/Shopping/BundleService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Payments.API.Dtos.ShoppingDtos;
+using Explorer.Payments.API.Dtos.WalletDtos;
+using Explorer.Payments.API.Public.Shopping;
+using Explorer.Payments.Core.Domain.RepositoryInterfaces;
+using Explorer.Payments.Core.Domain.Shopping;
+using Explorer.Payments.Core.Domain.Wallets;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Payments.Core.UseCases.Shopping
+{
+    public class BundleService : CrudService<BundleDto, Bundle>, IBundleService
+    {
+        private readonly IBundleRepository bundleRepository;
+        public BundleService(IBundleRepository bundleRepository, IMapper mapper) : base(bundleRepository, mapper)
+        {
+            this.bundleRepository = bundleRepository;
+        }
+
+        public Result<List<BundleDto>> GetByAuthorId(int auhtorId)
+        {
+            List<Bundle>? bundles = bundleRepository.GetByAuthorId(auhtorId);
+            if (bundles == null)
+            {
+                return Result.Fail<List<BundleDto>>("No bundle found for the specified author ID.");
+            }
+            return MapToDto(bundles);
+        }
+    }
+}
