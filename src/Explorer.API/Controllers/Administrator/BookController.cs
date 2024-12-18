155cb97294558c4c321853ff90d0fe2a9daf5e07
mimasekaric <mimasekaric@gmail.com>
2024-12-18 16:09:22 +0100

    Create book

---
+++ src/Explorer.API/Controllers/Administrator/BookController.cs
@@ -0,0 +1,30 @@
+﻿using Explorer.Encounters.API.Dtos.SecretsDtos;
+using Explorer.Encounters.API.Public;
+using Explorer.Stakeholders.Infrastructure.Authentication;
+using Explorer.Tours.API.Dtos.TourLifeCycleDtos;
+using Explorer.Tours.Core.Domain.Tours;
+using FluentResults;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace Explorer.API.Controllers.Administrator
+{
+    [Authorize(Policy = "administratorPolicy")]
+    [Route("api/admin/books")]
+    public class BookController : BaseApiController
+    {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpPost("createBook")]
+        public ActionResult<BookDto> Create([FromBody] BookDto book)
+        {
+            book.AdminId = User.PersonId();
+            var result = _bookService.Create(book);
+            return CreateResponse(result);      
+        }
+    }
+}
