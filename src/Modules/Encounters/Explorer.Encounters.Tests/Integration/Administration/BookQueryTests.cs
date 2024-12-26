2f4b9a731cbd7a9bfe0168b465c5510f463672f4
mimasekaric <mimasekaric@gmail.com>
2024-12-26 16:03:13 +0100

    Tests for encounter's stories and books
     all tests are passing

---
+++ src/Modules/Encounters/Explorer.Encounters.Tests/Integration/Administration/BookQueryTests.cs
@@ -0,0 +1,45 @@
+﻿using Explorer.API.Controllers.Administrator;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Encounters.API.Dtos.SecretsDtos;
+using Explorer.Encounters.API.Public;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.Tests.Integration.Administration
+{
+    [Collection("Sequential")]
+    public class BookQueryTests : BaseEncountersIntegrationTest
+    {
+        public BookQueryTests(EncountersTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void Retrieves_all()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+
+            // Act
+            var result = ((ObjectResult)controller.GetAll().Result)?.Value as PagedResult<BookDto>;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.Results.Count.ShouldBe(1);
+            result.TotalCount.ShouldBe(1);
+        }
+
+        private static BookController CreateController(IServiceScope scope)
+        {
+            return new BookController(scope.ServiceProvider.GetRequiredService<IBookService>())
+            {
+                ControllerContext = BuildContext("-1")
+            };
+        }
+    }
+}
---
+++ src/Modules/Encounters/Explorer.Encounters.Tests/Integration/Authoring/StoryQueryTests.cs
@@ -0,0 +1,47 @@
+﻿using Explorer.API.Controllers.Author.Authoring;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Encounters.API.Dtos.SecretsDtos;
+using Explorer.Encounters.API.Public;
+using Explorer.Tours.API.Public.Authoring;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.Tests.Integration.Authoring
+{
+    [Collection("Sequential")]
+    public class StoryQueryTests : BaseEncountersIntegrationTest
+    {
+        public StoryQueryTests(EncountersTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void Retrieves_all()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+
+            // Act
+            var result = ((ObjectResult)controller.GetAll().Result)?.Value as PagedResult<StoryDto>;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.Results.Count.ShouldBe(1);
+            result.TotalCount.ShouldBe(1);
+        }
+
+        private static StoryController CreateController(IServiceScope scope)
+        {
+            return new StoryController(scope.ServiceProvider.GetRequiredService<IStoryService>(), scope.ServiceProvider.GetRequiredService<IPublishRequestService>(), scope.ServiceProvider.GetRequiredService<IKeyPointService>())
+            {
+                ControllerContext = BuildContext("-1")
+            };
+        }
+    }
+
+}
